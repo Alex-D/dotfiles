a7484d994fb04d4ad74197b650f35215c4012b6e
Alex-D <contact@alex-d.fr>
2018-12-15 00:46:38 +0100

    fix: remove hyper-tabs-enhanced plugin to avoid CPU usage issue

--- .hyper.js
+++ .hyper.js
@@ -135,7 +135,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ["hyper-material-theme", "hyper-search", "hyper-tabs-enhanced"],
+  plugins: ["hyper-material-theme", "hyper-search"],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
