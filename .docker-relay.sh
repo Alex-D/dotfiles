b8882517e2d6f0c98424612d87054de1de45147a
Alex-D <contact@alex-d.fr>
2019-03-20 19:15:40 +0100

    add comment to the article in docker-relay

--- .docker-relay.sh
+++ .docker-relay.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# See https://blogs.technet.microsoft.com/virtualization/2017/12/08/wsl-interoperability-with-docker/
+
 if [ "$(id -u)" != "0"  ]; then
   echo "This script must be run as root" 1>&2
   exit 1
