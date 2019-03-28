6212fa9be56e90ffe2682ec34de04893bd505bee
Alex-D <contact@alex-d.fr>
2019-03-28 01:32:55 +0100

    fix some ls color issues with folders

--- .aliases.zsh
+++ .aliases.zsh
@@ -3,7 +3,7 @@ alias ...="cd ../.."
 alias ....="cd ../../.."
 alias .....="cd ../../../.."
 
-alias ls='ls --color'
+alias ls='ls --color=auto'
 alias lsa='ls -lah'
 alias l='ls -lah'
 alias ll='ls -lh'
