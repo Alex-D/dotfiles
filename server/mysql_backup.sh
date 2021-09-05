65a2d959bc749073adbca77c9d082306482436ed
Alex-D <contact@alex-d.fr>
2021-09-06 00:05:42 +0200

    fix: fix mysql backup script parameter usage

--- server/mysql_backup.sh
+++ server/mysql_backup.sh
@@ -7,6 +7,8 @@ set -xe
 #   DB_PWD: Password
 #   1st argument: Database name
 
+DB_NAME=$1
+
 CONFIG_PATH="$HOME/.config/backup/mysql"
 . "$CONFIG_PATH"
 
@@ -14,7 +16,7 @@ TMP_DIR="/tmp"
 DUMP_PATH="$TMP_DIR/$DB_NAME.sql.gz"
 DROPBOX_UPLOADER_CONFIG_PATH="$HOME/.dropbox_uploader"
 
-MYSQL_PWD="$DB_PWD" mysqldump -u"$DB_USER" "$1" | gzip > "$DUMP_PATH"
+MYSQL_PWD="$DB_PWD" mysqldump -u"$DB_USER" "$DB_NAME" | gzip > "$DUMP_PATH"
 
 dropbox_uploader -f "$DROPBOX_UPLOADER_CONFIG_PATH" upload "$DUMP_PATH" /
 
